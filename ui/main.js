32deb62b05a502a76486be7f5af6b45ec29b4f23
kedartgokhale <30648860+kedartgokhale@users.noreply.github.com>
2017-08-14 18:58:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var  button =document.getElementById('bt1');
+var counter =0;
+button.onclick=function(){
+    counter=counter+1;
+    var span =document.getElementById('sp1');
+    span.innerHTML=counter.toString();
+};
