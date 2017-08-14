a2745e280593fdd6321703be4001ae6444b61415
kedartgokhale <30648860+kedartgokhale@users.noreply.github.com>
2017-08-14 18:40:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ var articles={
 var counter=0;
 app.get('/counter', function (req,res) {
     counter=counter+1;
-    res.send(counter.to.String());
+    res.send(counter.toString());
 });
 
 app.get('/', function (req, res) {
