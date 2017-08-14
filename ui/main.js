1c6555d2d38572b155b13cb78aac8ca29573e7bc
kedartgokhale <30648860+kedartgokhale@users.noreply.github.com>
2017-08-14 19:30:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,22 @@
 var  button =document.getElementById('bt1');
-var counter =0;
+
 button.onclick=function(){
-    counter=counter+1;
-    var span =document.getElementById('sp1');
-    span.innerHTML=counter.toString();
+    //create a req obj
+    var req= new XMLHttpRequest();
+    //capture the response and store it in variable
+    req.onreadystatechange=function()
+    {
+        if(req.readyState===XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(req=status===200){
+                var couter = req.responseText;
+                var span =document.getElementById('sp1');
+                span.innerHTML=counter.toString();
+            }
+        }
+        };   
+            //make the request
+    req.open('GET','http://gkedartushar.imad.hasura-app.io/counter',true);
+    req.send(null);
 };
